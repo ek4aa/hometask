8f0bd0034a933a4e1a6521979838e95f5097500d
unknown <wintan1418@gmail.com>
2022-08-04 18:55:10 +0100

    Refactor

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,25 +1,28 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
+
   def index
     @room = Room.new
     @rooms = Room.public_rooms
-    @users = User.all_except(current_user)
-  end
 
-  def create
-    @room = Room.create(name: params['room']['name'])
+    @users = User.all_except(current_user)
+    render 'index'
   end
 
   def show
     @single_room = Room.find(params[:id])
-    current_user.update_without_password(room_id: @single_room.id)
 
-    @rooms = Room.public_rooms
-    @users = User.all_except(current_user)
     @room = Room.new
-    @message = Message.new
+    @rooms = Room.public_rooms
 
+    @message = Message.new
     @messages = @single_room.messages.order(created_at: :asc)
+
+    @users = User.all_except(current_user)
     render 'index'
+  end
+
+  def create
+    @room = Room.create(name: params['room']['name'])
   end
 end
